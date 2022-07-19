6b1e0d5b8b8efec06ab9073759bab19674fd6fef
Ericchuks <you@example.com>
2022-07-19 11:08:19 +0100

    updated

--- script.js
+++ script.js
@@ -3,16 +3,12 @@ let close = document.querySelector(".close");
 let contentBox = document.querySelector(".opac");
 let modal = document.querySelector(".modal-container");
 let modalImg = document.querySelector(".modal section img:first-child");
-let shoeDescription = document.querySelector(".description");
-let priceTag = document.querySelector(".price-tag");
-let dustBin = document.querySelector(".modal section img:last-child");
 let checkOut = document.querySelector(".modal button");
 let cart = document.querySelector(".cart");
 let addToCart = document.querySelector(".addToCart");
-let modalValue = "Autumn Limited Edition...";
 let plus = document.querySelector(".plus");
 let minus = document.querySelector(".minus");
-let amount =document.querySelector(".amount");
+let amount = document.querySelector(".amount");
 let cartItem = document.querySelector(".cartItems");
 
 console.log(cartItem);
@@ -40,7 +36,7 @@ function openCart(){
 
 cart.addEventListener("click", openCart);
 
- let a = 0;
+ let a = 1;
 function increase(){
     a++;
     amount.innerHTML = a;
@@ -55,6 +51,7 @@ minus.addEventListener("click", () => {
     }
 })
 
+
 function addToModal(){
     let section = document.createElement("section");
     let img = document.createElement("img");
@@ -68,6 +65,69 @@ function addToModal(){
     span2.className = "number"
     let b = document.createElement("b");
     b.className = "totals"
+    let dustBin = document.createElement("img");
+    dustBin.setAttribute("src", "images/icon-delete.svg");
+    let hasItem = false;
+    cartItem.prepend(section);
+    section.appendChild(img);
+    section.appendChild(summary);
+    summary.appendChild(p);
+    summary.appendChild(span);
+    summary.appendChild(span2);
+    summary.appendChild(b);
+    p.textContent = "Autumn Limited Edition...";
+    span.textContent = `${this.parentElement.childNodes[7].childNodes[1].childNodes[1].textContent}`;
+    span2.textContent = ` x ${a}`;
+    let spanNumber = parseInt(span.textContent) * parseInt(a);
+
+    b.textContent = `  $${spanNumber}.00`;
+    section.appendChild(dustBin)
+    console.log("000");
+    // NOT REPETING OF PRODUUCT ALREADY IN CART BUT INCREASE FOR THAT SAME PRODUCT (HAVEN'T GOTTEN THE LOGIC YET)
+    if(section.parentElement.childNodes.textContent == true){
+        console.log(section.parentElement)
+    }else{
+        console.log("222")
+    };
+function del(){
+    this.parentElement.style.display = "none";
+    console.log("222");
+}
+dustBin.addEventListener("click", del);
 }
 
-addToModal();
+addToCart.addEventListener("click", addToModal);
+
+
+
+let previous = document.querySelector(".previous");
+let next = document.querySelector(".next");
+let boxImg = document.querySelectorAll(".big img");
+let slideIndex = 1;
+showSlides(slideIndex);
+
+    // NEXT / PREVIOUS CONTROL
+function plusBox(n){
+    showSlides(slideIndex += n);
+}
+//     // IMAGE CONTROL
+function currentSlide(n){
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+    if(n > boxImg.length){
+        slideIndex = 1;
+    }
+    if(n < 1){
+        slideIndex = boxImg.length
+    }
+    for(let i = 0; i < boxImg.length; i++){
+        boxImg[i].style.display = "none";
+    }
+    boxImg[slideIndex-1].style.display = "block";
+}
+
+
+next.addEventListener("click", plusBox(1));
+previous.addEventListener("click", plusBox(-1));
